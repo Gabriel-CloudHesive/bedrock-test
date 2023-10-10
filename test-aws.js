809e0c5ad0716a3cfab7b37eafd172aadbb99147
gabriel-RM95 <gabriel.rodriguezm95@gmail.com>
2023-10-10 18:21:20 -0400

    chatbot pending

--- test-aws.js
+++ test-aws.js
@@ -1,11 +1,36 @@
-const { BedrockRuntimeClient, InvokeModelCommand } = require("@aws-sdk/client-bedrock-runtime");
+/* 
+    Text Generation.
+    Using only AWS SDK.
+    zero-shot prompt.
+*/
+
+import { BedrockClient, ListFoundationModelsCommand } from "@aws-sdk/client-bedrock";
+import { BedrockRuntimeClient, InvokeModelCommand } from "@aws-sdk/client-bedrock-runtime";
+
+/* **************************List Foundation Models************************************** */
+/* const bedrockClient = new BedrockClient({ region: "us-east-1" });
+const bedrockInput = { // ListFoundationModelsRequest
+    // byProvider: "STRING_VALUE",
+    // byCustomizationType: "FINE_TUNING",
+    // byOutputModality: "TEXT" || "IMAGE" || "EMBEDDING",
+    // byInferenceType: "ON_DEMAND" || "PROVISIONED",
+};
+
+const bedrockCommand = new ListFoundationModelsCommand(bedrockInput);
+bedrockClient.send(bedrockCommand)
+    .then((value) => {
+        console.log(value);
+    }); */
+
+/* **************************Invoke Model Command************************************** */
 const client = new BedrockRuntimeClient({ region: "us-east-1" });
 const input = {
-    body: `{ "prompt": "a customer writes an email \“i would like a refund\” please help me write a response to sent in email", "maxTokens": 200, "temperature": 0 }`, // required
+    body: `{ "prompt": "a customer writes an email \“i would like a refund\” please help me write a response to sent in email.", "maxTokens": 200, "temperature": 0 }`, // required
     contentType: "application/json",
     accept: "*/*",
     modelId: "ai21.j2-mid-v1",
 };
+
 const command = new InvokeModelCommand(input);
 client.send(command)
     .then((value) => {
@@ -15,5 +40,3 @@ client.send(command)
     }, (reason) => {
         console.log(reason);
     });
-
-
--- test-langchain.js
+++ test-langchain.js
@@ -1,15 +1,39 @@
 /* 
-    Using LangChain
+    Text Generation.
+    Using LangChain.
     https://js.langchain.com/docs/modules/model_io/models/llms/integrations/bedrock
+    contextual generation.
 */
 
-var { Bedrock } = require("langchain/llms/bedrock")
-// If no credentials are provided, the default credentials from
-// @aws-sdk/credential-provider-node will be used.
+import { Bedrock } from "langchain/llms/bedrock";
+import { PromptTemplate } from 'langchain/prompts';
+
+const template = `Human: Create an apology email from the Service Manager {manager} to {customer} in response to the following feedback that was received from the customer: 
+<customer_feedback>
+{feedback}
+</customer_feedback>
+
+Assistant:`;
+
+const promptTemplate = new PromptTemplate({
+    template: template,
+    inputVariables: ["manager", "customer", "feedback"]
+});
+
+const formattedPrompt = await promptTemplate.format({
+    manager: "Bob",
+    customer: "John Doe",
+    feedback: `Hello Bob,
+    I am very disappointed with the recent experience I had when I called your customer support.
+    I was expecting an immediate call back but it took three days for us to get a call back.
+    The first suggestion to fix the problem was incorrect. Ultimately the problem was fixed after three days.
+    We are very unhappy with the response provided and may consider taking our business elsewhere.`
+});
+
 const model = new Bedrock({
     model: "ai21.j2-mid-v1",
     region: "us-east-1",
-    // endpointUrl: "custom.amazonaws.com",
+    // endpointUrl: "custom.amazonaws.com", 
     // credentials: {
     //   accessKeyId: process.env.BEDROCK_AWS_ACCESS_KEY_ID!,
     //   secretAccessKey: process.env.BEDROCK_AWS_SECRET_ACCESS_KEY!,
@@ -17,6 +41,5 @@ const model = new Bedrock({
     // modelKwargs: {},
 });
 
-model.invoke("a customer writes an email \“i would like a refund\” please help me write a response to sent in email")
-    .then((value) => { console.log('Response: ', value); })
-    .catch((reason) => { console.log('Error: ', reason); });
+var response = await model.invoke(formattedPrompt);
+console.log(response);
